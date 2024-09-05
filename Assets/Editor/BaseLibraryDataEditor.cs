6cd435c7e6fd8c875e46ee089223b0999a636ec9
gwong3dsnk <gwongkmst@gmail.com>
2024-09-05 16:41:36 -0700

    Added text wrapping to label description

--- Assets/Editor/BaseLibraryDataEditor.cs
+++ Assets/Editor/BaseLibraryDataEditor.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 
 public abstract class BaseLibraryDataEditor<T> : Editor where T : ScriptableObject
 {
+    private GUIStyle labelTextWrapping;
     protected string abilityNameFilePath = Path.Combine(Application.dataPath, "Editor/Temp/abilityNames.txt");
 
     public override void OnInspectorGUI()
     {
+        labelTextWrapping = new GUIStyle(EditorStyles.label)
+        {
+            wordWrap = true
+        };
+
         DrawDefaultInspector();
 
-        T libraryData = (T)target;
+        T libraryData = (T)target; 
 
         // Design the GUI
         EditorGUILayout.LabelField("Ability Name Management", EditorStyles.boldLabel);
-        EditorGUILayout.LabelField("Use the buttons below to manage ability names.");        
+        EditorGUILayout.LabelField("Use the buttons below, in order, whenever adding/renaming/deleting ability prefabs from Assets/PlayerAbilities/Prefabs.", labelTextWrapping);
 
-        GUIContent saveAbilityNamebuttonContent = new GUIContent("Save Ability Names", 
+        GUIContent saveAbilityNamebuttonContent = new GUIContent("1. Save Ability Names", 
             "Click this to save the current ability name assignments");
-        GUIContent buttonContent = new GUIContent("Regenerate Ability Names", 
+        GUIContent buttonContent = new GUIContent("2. Regenerate Ability Names", 
             "Click this whenever adding/deleting/modifying ability prefabs in Assets/PlayerAbilities/Prefabs");
-        GUIContent loadAbilityNamebuttonContent = new GUIContent("Load Ability Names", 
+        GUIContent loadAbilityNamebuttonContent = new GUIContent("3. Load Ability Names", 
             "Click this to load the correct ability name assignments");            
 
         // Establish method calls on button press
